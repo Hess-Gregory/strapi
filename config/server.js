8199f6ba01945e53bed8b2ce44dcef753740962e
Gregory HESS <hessgregory@hotmail.com>
2022-12-07 01:04:09 +0100

    config file .env
    npm run  start with ./server.js in file package.json
    add url in file config/server.js

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', '127.0.0.1'),
   port: env.int('PORT', 1337),
+  url: env.int('PUBLIC_URL', 'http://yourbackend.com'),
   app: {
     keys: env.array('APP_KEYS'),
   },
